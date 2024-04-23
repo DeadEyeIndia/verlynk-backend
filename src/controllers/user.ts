b5a2342e5beb4a722dff611042edc8f6877e40db
DeadEyeIndia <rahulbattin24@gmail.com>
2024-04-23 15:54:49 +0530

    User Info Edit & User Password Change API Routes

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,11 +3,19 @@ import { ObjectId } from "mongodb";
 
 import { getMongoClient } from "../models/mongodb";
 import { IUser } from "../models/user";
-import { findUserByEmail } from "../lib/data/user";
+import { IPost } from "../models/post";
+import { IComment } from "../models/comment";
+import { findUserByEmail, findUserById } from "../lib/data/user";
 import catchAsyncError from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/errorHandling";
-import { hashPassword } from "../utils/bcrypt";
-import { DB_NAME, pick, USER_COLLECTION } from "../utils/constants";
+import { comparePassword, hashPassword } from "../utils/bcrypt";
+import {
+  COMMENT_COLLECTION,
+  DB_NAME,
+  pick,
+  POST_COLLECTION,
+  USER_COLLECTION,
+} from "../utils/constants";
 
 /**
  * User controller function for creating a new user.
@@ -76,7 +84,8 @@ export const getUser = catchAsyncError(
 
     const existingUser = await findUserByEmail(db, req.user.email);
     if (!existingUser) {
-      return next(new ErrorHandler("User not found in getUser", 404));
+      (await client).close();
+      return next(new ErrorHandler("User not found", 404));
     }
 
     (await client).close();
@@ -84,5 +93,174 @@ export const getUser = catchAsyncError(
     const user = pick(existingUser, "_id", "email", "fullname", "createdAt");
 
     return res.status(200).json(user);
+  }
+);
+
+/**
+ * User controller edit user information based on the email address associated with the request user.
+ * @param {Request<{}, {}, {email: string; fullname: string}>} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next function to be called in the middleware stack.
+ * @returns {Promise<void>} A promise that resolves once the operation is complete.
+ */
+export const editUser = catchAsyncError(
+  async (
+    req: Request<{}, {}, { email: string; fullname: string }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { email, fullname } = req.body;
+    if (!email || !fullname || email === "" || fullname === "") {
+      return next(new ErrorHandler("Missing Fields!", 406));
+    }
+
+    const { client } = await getMongoClient();
+    const db = (await client).db(DB_NAME);
+
+    const existingUser = await findUserByEmail(db, req.user.email);
+    if (!existingUser) {
+      (await client).close();
+      return next(new ErrorHandler("User not found", 404));
+    }
+    if (existingUser._id.toString() === req.user._id.toString()) {
+      (await client).close();
+      return next(new ErrorHandler("Not authorized", 401));
+    }
+
+    const updatedNow = new Date();
+    const { acknowledged } = await db
+      .collection<IUser>(USER_COLLECTION)
+      .updateOne(
+        { _id: existingUser._id },
+        { $set: { email: email, fullname: fullname, updatedAt: updatedNow } }
+      );
+    (await client).close();
+    if (!acknowledged) {
+      return next(new ErrorHandler("Something went wrong", 400));
+    }
+
+    return res.status(201).json({
+      success: acknowledged,
+    });
+  }
+);
+
+/**
+ * User controller edit user password based on the email address associated with the request user.
+ * @param {Request<{}, {}, {email: string; fullname: string}>} req - The request object.
+ * @param {Response<{success: boolean}>} res - The response object with success.
+ * @param {NextFunction} next - The next function to be called in the middleware stack.
+ * @returns {Promise<void>} A promise that resolves once the operation is complete.
+ */
+export const editUserPassword = catchAsyncError(
+  async (
+    req: Request<{}, {}, { password: string }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { password } = req.body;
+    if (!password || password === "") {
+      return next(new ErrorHandler("Missing Fields!", 406));
+    }
+
+    const { client } = await getMongoClient();
+    const db = (await client).db(DB_NAME);
+
+    const existingUser = await findUserByEmail(db, req.user.email);
+    if (!existingUser) {
+      (await client).close();
+      return next(new ErrorHandler("User not found", 404));
+    }
+
+    if (existingUser._id.toString() === req.user._id.toString()) {
+      (await client).close();
+      return next(new ErrorHandler("Not authorized", 401));
+    }
+
+    const isMatching = await comparePassword(password, existingUser.password);
+
+    if (isMatching) {
+      (await client).close();
+      return next(new ErrorHandler("Try different password", 406));
+    }
+
+    const hashedPass = await hashPassword(password);
+    const updatedNow = new Date();
+
+    const { acknowledged } = await db
+      .collection<IUser>(USER_COLLECTION)
+      .updateOne(
+        { _id: existingUser._id },
+        { $set: { password: hashedPass, updatedAt: updatedNow } }
+      );
+
+    (await client).close();
+    if (!acknowledged) {
+      return next(
+        new ErrorHandler("Password not changed, try again later", 400)
+      );
+    }
+
+    res.status(201).json({
+      success: acknowledged,
+    });
+  }
+);
+
+/**
+ * User controller delete user information based on the user id associated with the request user.
+ * @param {Request<{userid: string}>} req - The request object.
+ * @param {Response<{userdeleted: boolean; commentAcknowledged: boolean; commentsDeleted: number; postAcknowledged: boolean; postsDeleted: boolean; postsDeleted: number}>} res - The response object containing user deleted, user comments deleted and user posts deleted.
+ * @param {NextFunction} next - The next function to be called in the middleware stack.
+ * @returns {Promise<void>} A promise that resolves once the operation is complete.
+ */
+export const deleteUser = catchAsyncError(
+  async (
+    req: Request<{ userid: string }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { userid } = req.params;
+
+    if (userid === undefined) {
+      return next(new ErrorHandler("Please provide a userid", 406));
+    }
+
+    if (!ObjectId.isValid(userid)) {
+      return next(new ErrorHandler("Not valid user Id", 406));
+    }
+
+    const { client } = await getMongoClient();
+    const db = (await client).db(DB_NAME);
+    const existingUser = await findUserById(db, userid);
+    if (!existingUser) {
+      (await client).close();
+      return next(new ErrorHandler("User not found", 404));
+    }
+
+    const { acknowledged: postAcknowledged, deletedCount: postsDeleted } =
+      await db
+        .collection<IPost>(POST_COLLECTION)
+        .deleteMany({ author: existingUser._id });
+    const { acknowledged: commentAcknowledged, deletedCount: commentsDeleted } =
+      await db
+        .collection<IComment>(COMMENT_COLLECTION)
+        .deleteMany({ user: existingUser._id });
+    const { acknowledged } = await db
+      .collection<IUser>(USER_COLLECTION)
+      .deleteOne({ _id: existingUser._id });
+
+    (await client).close();
+    if (!postAcknowledged || !commentAcknowledged || !acknowledged) {
+      return next(new ErrorHandler("User not completely deleted", 400));
+    }
+
+    res.status(201).json({
+      userdeleted: acknowledged,
+      commentAcknowledged: commentAcknowledged,
+      commentsDeleted: commentsDeleted,
+      postAcknowledged: postAcknowledged,
+      postsDeleted: postsDeleted,
+    });
   }
 );
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,12 @@
 import express from "express";
 
-import { userSignUp, getUser } from "../controllers/user";
+import {
+  userSignUp,
+  getUser,
+  editUser,
+  editUserPassword,
+  deleteUser,
+} from "../controllers/user";
 
 import { isAuthenticatedUser } from "../middleware/auth";
 import rateLimit from "express-rate-limit";
@@ -11,6 +17,8 @@ import rateLimit from "express-rate-limit";
  * USER SIGNUP - /signup
  *
  * USER PROFILE[ME] - /me
+ *
+ * USER EDIT(INFO) - /edit
  */
 const router = express.Router();
 
@@ -23,5 +31,34 @@ router.route("/signup").post(
   userSignUp
 );
 router.route("/me").get(isAuthenticatedUser, getUser);
+router.route("/edit").patch(
+  rateLimit({
+    limit: 10,
+    windowMs: 6 * 60 * 60 * 1000,
+    message: "Too many requests, please try again later",
+  }),
+  isAuthenticatedUser,
+  editUser
+);
+router.route("/edit/password").patch(
+  rateLimit({
+    limit: 10,
+    windowMs: 6 * 60 * 60 * 1000,
+    message: "Too many requests, please try again later",
+  }),
+  isAuthenticatedUser,
+  editUserPassword
+);
+
+// Comment or Uncomment below route for deleting user by userId which will delete all posts and comments from that user.
+router.route("/delete/:userid").delete(
+  rateLimit({
+    limit: 1000,
+    windowMs: 6 * 60 * 60 * 1000,
+    message: "Too many requests, please try again later",
+  }),
+  isAuthenticatedUser,
+  deleteUser
+);
 
 export default router;
